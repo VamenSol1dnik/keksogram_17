5d4cd267e2add13f4fba061bc08cb6e989727dc1
VamenSol1dnik <valeravas03@gmail.com>
2023-05-10 23:27:31 +0300

    find bugs

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,3 +1,5 @@
+const lightbox = document.querySelector(".big-picture");
+const bigImg = document.querySelector(".big-picture__img img");
 const btnCloseLightbox = document.querySelector("#picture-cancel");
 const btnLike = document.querySelector(".likes-count");
 const commentsCounter = document.querySelector(".comments-count");
@@ -5,39 +7,72 @@ const commentsShown = document.querySelector(".comments-shown");
 const commentsParent = document.querySelector(".social__comments");
 const photoPostAuthor = document.querySelector(".social__picture");
 const photoDescription = document.querySelector(".social__caption");
+const btnMoreComments = document.querySelector(".social__comments-loader");
+
+
 
 
 export function openBigImage(target, posts){
   const id = Number(target.dataset.picid);
-
   lightbox.classList.remove("hidden");
   toggleBodyScroll("noscroll");
-
   const postData = posts.find(post => post.id === id);
   bigImg.src = postData.url;
-
   photoPostAuthor.src = postData.avatar;
   photoDescription.innerText = postData.description;
   btnLike.innerText = postData.likes;
   commentsCounter.innerText = postData.comments.length;
   commentsShown.innerText = postData.comments.length;
 
-  const socialComment = document.querySelector(".social__comment");
+  const socialComment = document.querySelector("#social__comment");
 
   const commentsFragment = new DocumentFragment();
 
-  postData.comments.forEach(comment => {
-    let tmpSocialComment = socialComment.cloneNode(true);
-
-    tmpSocialComment.querySelector(".social__author").innerText = comment.name;
-    tmpSocialComment.querySelector(".social__text").innerText = comment.message;
-    tmpSocialComment.querySelector(".social__picture").src = comment.avatar;
-
-    commentsFragment.append(tmpSocialComment);
-  })
-
   commentsParent.innerHTML = '';
-  commentsParent.append(commentsFragment);
+
+  const commentsPackQuantity = 5;
+  const commentsQuantity = postData.comments.length;
+  let commentsPosted = 0;
+  let commentsPack = postData.comments.slice();
+
+  addComments();
+  if (commentsQuantity > commentsPackQuantity) {
+    btnMoreComments.addEventListener("click", addComments);
+  }
+  function addComments() {
+    addCommentsPack(commentsPack.slice(commentsPosted, commentsPosted + commentsPackQuantity));
+    commentsShown.innerText = commentsPosted;
+
+    if (commentsPosted < commentsQuantity) {
+      showMoreCommentsBtn();
+    }
+    else {
+      hideMoreCommentsBtn()
+    }
+  }
+
+  function hideMoreCommentsBtn(){
+    btnMoreComments.classList.add("hidden");
+  }
+  function showMoreCommentsBtn(){
+    btnMoreComments.classList.remove("hidden");
+  }
+
+  function addCommentsPack(comments){
+    comments.forEach(comment => {
+      let tmpSocialComment = socialComment.content.cloneNode(true);
+
+      tmpSocialComment.querySelector(".social__author").innerText = comment.name;
+      tmpSocialComment.querySelector(".social__text").innerText = comment.message;
+      tmpSocialComment.querySelector(".social__picture").src = comment.avatar;
+
+      commentsFragment.append(tmpSocialComment);
+
+      commentsPosted++;
+    })
+
+    commentsParent.append(commentsFragment);
+  }
 
   btnCloseLightbox.addEventListener("click", closeBigImage);
 
